d2d008f7f916684ee751c7b2a64a4c269faf3126
Jeremy Karlsson <karlsson@jeremy.se>
2015-09-09 13:06:26 +0200

    Added Social Reach class

--- social-reach-plugin-updater.php
+++ social-reach-plugin-updater.php
@@ -39,7 +39,7 @@ class Social_Reach_Plugin_Updater {
 
   private function get_repository_info() {
     if (is_null($this->github_response)) {
-      $request_uri = sprintf('http://api.github.com/respos/%s/%s/releases', $this->username, $this->repository)
+      $request_uri = sprintf('http://api.github.com/respos/%s/%s/releases', $this->username, $this->repository);
 
       if ($this->authorize_token) {
         $request_uri = add_query_arg('access_token', $this->authorize_token, $request_uri);
@@ -58,75 +58,75 @@ class Social_Reach_Plugin_Updater {
       $this->github_response = $response;
     }
   }
-}
 
-public function modify_transient($transient) {
-  if (property_exists( $transient, 'checked')) {
-    if ($checked = $transient->checked) {
-      $this->get_repository_info();
-      $out_of_date = version_compare($this->github_response['tag_name'], $checked[$this->basename], 'gt');
-      
-      if ($out_of_date) {
-        $new_files = $this->github_response['zipball_url'];
-        $slug = current(explode('/', $this->basename ));
-        $plugin = array(
-          'url' => $this->plugin["PluginURI"],
-          'slug' => $slug,
-          'package' => $new_files,
-          'new_version' => $this->github_response['tag_name']
-        );
-        $transient->response[$this->basename] = (object) $plugin;
+  public function modify_transient($transient) {
+    if (property_exists( $transient, 'checked')) {
+      if ($checked = $transient->checked) {
+        $this->get_repository_info();
+        $out_of_date = version_compare($this->github_response['tag_name'], $checked[$this->basename], 'gt');
+        
+        if ($out_of_date) {
+          $new_files = $this->github_response['zipball_url'];
+          $slug = current(explode('/', $this->basename ));
+          $plugin = array(
+            'url' => $this->plugin["PluginURI"],
+            'slug' => $slug,
+            'package' => $new_files,
+            'new_version' => $this->github_response['tag_name']
+          );
+          $transient->response[$this->basename] = (object) $plugin;
+        }
       }
     }
+    return $transient;
   }
-  return $transient;
-}
 
-public function plugin_popup( $result, $action, $args ) {
-  if (!empty( $args->slug )) {
-    if ($args->slug == $this->basename) {
-      $this->get_repository_info();
+  public function plugin_popup( $result, $action, $args ) {
+    if (!empty( $args->slug )) {
+      if ($args->slug == $this->basename) {
+        $this->get_repository_info();
 
-      $plugin = array(
-        'name'              => $this->plugin["Name"],
-        'slug'              => $this->basename,
-        'version'           => $this->github_response['tag_name'],
-        'author'            => $this->plugin["AuthorName"],
-        'author_profile'    => $this->plugin["AuthorURI"],
-        'last_updated'      => $this->github_response['published_at'],
-        'homepage'          => $this->plugin["PluginURI"],
-        'short_description' => $this->plugin["Description"],
-        'sections'          => array( 
-            'Description'   => $this->plugin["Description"],
-            'Updates'       => $this->github_response['body'],
-        ),
-        'download_link'     => $this->github_response['zipball_url']
-      );
+        $plugin = array(
+          'name'              => $this->plugin["Name"],
+          'slug'              => $this->basename,
+          'version'           => $this->github_response['tag_name'],
+          'author'            => $this->plugin["AuthorName"],
+          'author_profile'    => $this->plugin["AuthorURI"],
+          'last_updated'      => $this->github_response['published_at'],
+          'homepage'          => $this->plugin["PluginURI"],
+          'short_description' => $this->plugin["Description"],
+          'sections'          => array( 
+              'Description'   => $this->plugin["Description"],
+              'Updates'       => $this->github_response['body'],
+          ),
+          'download_link'     => $this->github_response['zipball_url']
+        );
 
-      return (object) $plugin;
+        return (object) $plugin;
+      }
+    }   
+    return $result;
+  }
+
+  public function after_install($response, $hook_extra, $result) {
+    global $wp_filesystem;
+
+    $install_directory = plugin_dir_path($this->file);
+    $wp_filesystem->move($result['destination'], $install_directory);
+    $result['destination'] = $install_directory;
+     
+    if ($this->active) {
+      activate_plugin($this->basename);
     }
-  }   
-  return $result;
-}
-
-public function after_install($response, $hook_extra, $result) {
-  global $wp_filesystem;
-
-  $install_directory = plugin_dir_path($this->file);
-  $wp_filesystem->move($result['destination'], $install_directory);
-  $result['destination'] = $install_directory;
-   
-  if ($this->active) {
-    activate_plugin($this->basename);
+    
+    return $result;
   }
-  
-  return $result;
-}
 
-public function initialize() {
-  add_filter('pre_set_site_transient_update_plugins', array($this, 'modify_transient'), 10, 1);
-  add_filter('plugins_api', array($this, 'plugin_popup'), 10, 3);
-  add_filter('upgrader_post_install', array($this, 'after_install'), 10, 3);
+  public function initialize() {
+    add_filter('pre_set_site_transient_update_plugins', array($this, 'modify_transient'), 10, 1);
+    add_filter('plugins_api', array($this, 'plugin_popup'), 10, 3);
+    add_filter('upgrader_post_install', array($this, 'after_install'), 10, 3);
+  }
 }
 
 ?>
--- social-reach.php
+++ social-reach.php
@@ -1,7 +1,40 @@
 <?php
 
 class Social_Reach() {
+	protected $url;
 
+	public function __constructor($url) {
+		$this->url = $url;
+	}
+
+	public function tweet_count() {
+		$json = file_get_contents('http://urls.api.twitter.com/1/urls/count.json?url=' . $this->url);
+		$arr = json_decode($json, true);
+
+		return intval($arr['count'])
+	}
+
+	public function facebook_likes_count() {
+		$json = file_get_contents('http://graph.facebook.com/?ids=' . $this->url);
+		$arr = json_decode($json, true);
+
+		return intval($json[$this->url]['shares']);
+	}
+
+	public function google_plus_one_count() {
+		$curl = curl_init();
+	    curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
+	    curl_setopt($curl, CURLOPT_POST, 1);
+	    curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $this->url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+	    $curl_results = curl_exec ($curl);
+	    curl_close ($curl);
+	 
+	    $json = json_decode($curl_results, true);
+	 
+	    return intval($json[0]['result']['metadata']['globalCounts']['count']);
+	}
 }
 
 ?>
